43606193130d31b698f7af654215b2cc24d0ed9e
Piotr Łukomiak <cieplak32@gmail.com>
2016-06-21 22:22:20 +0200

    update gitignore

---
+++ src/main/java/org/piotrek/spaceinvaders/dao/ScoreDaoImpl.java
@@ -0,0 +1,7 @@
+package org.piotrek.spaceinvaders.dao;
+
+/**
+ * Created by asus on 21.06.16.
+ */
+public class ScoreDaoImpl {
+}
---
+++ src/main/java/org/piotrek/spaceinvaders/model/Score.java
@@ -0,0 +1,7 @@
+package org.piotrek.spaceinvaders.model;
+
+/**
+ * Created by asus on 21.06.16.
+ */
+public class Score {
+}
